afbbe3bdda661ed18c28c764c28eaa5f91f95067
brodskiy_andrey <beer_ne@ukr.net>
2020-06-24 10:57:04 +0300

    pull request

--- Snowballs Battle/Assets/Sripts/GameElements/Shells/Snowboll/SnowbollEnemy.cs
+++ Snowballs Battle/Assets/Sripts/GameElements/Shells/Snowboll/SnowbollEnemy.cs
@@ -2,12 +2,13 @@
 
 namespace Assets.Sripts.GameElements.Shells.Snowboll
 {
-    class SnowbollEnemy : ShellModel
+    public class SnowbollEnemy : ShellModel
     { 
         public override void Initialization()
         {
             _rigidbody2D.bodyType = RigidbodyType2D.Static;
         }
+
         public override void DestroyElement()
         {
             gameObject.SetActive(false);
